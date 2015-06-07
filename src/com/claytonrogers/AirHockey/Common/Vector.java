f41cc79d34c75bef58b84b34bdc6498e99827c91
claytonrogers <claytonrogers53@gmail.com>
2015-06-06 20:30:01 -0400

    Renamed position class to vector.

--- src/com/claytonrogers/AirHockey/Common/Vector.java
+++ src/com/claytonrogers/AirHockey/Common/Vector.java
@@ -3,26 +3,26 @@ package com.claytonrogers.AirHockey.Common;
 /**
  * Created by clayton on 2015-06-06.
  */
-public class Position {
+public class Vector {
     public int x = 0;
     public int y = 0;
 
-    public Position() {}
-    public Position(int x, int y) {
+    public Vector() {}
+    public Vector(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    public Position(Position p) {
+    public Vector(Vector p) {
         x = p.x;
         y = p.y;
     }
 
-    public void add(Position p) {
+    public void add(Vector p) {
         x += p.x;
         y += p.y;
     }
 
-    public void sub(Position p) {
+    public void sub(Vector p) {
         x -= p.x;
         y -= p.y;
     }
