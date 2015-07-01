c0b4e0a4291cb8ca44da4a0b434fab481c923ea1
claytonrogers <claytonrogers53@gmail.com>
2015-07-01 14:05:04 -0400

    Increased the size of the player models to make it easier to hit the puck.

--- src/com/claytonrogers/AirHockey/Protocol/Protocol.java
+++ src/com/claytonrogers/AirHockey/Protocol/Protocol.java
@@ -4,13 +4,13 @@ package com.claytonrogers.AirHockey.Protocol;
  * Created by clayton on 2015-06-06.
  */
 public class Protocol {
-    public static String PROTOCOL_VERSION = "v0.4";
+    public static String PROTOCOL_VERSION = "v0.5";
     public static int PORT_NUMBER = 60046;
 
     public static int FIELD_WIDTH  = 600;
     public static int FIELD_HEIGHT = 900;
 
-    public static int PLAYER_RADIUS = 15;
+    public static int PLAYER_RADIUS = 30;
     public static int PUCK_RADIUS   = 10;
 
     public static boolean NET_DEBUG = false;
